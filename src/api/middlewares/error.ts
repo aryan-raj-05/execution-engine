11381478ff16f5538d449ecd0dc762705b05e06e
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-07-27 23:41:49 +0530

    TODO: error handlinig

---
+++ src/api/middlewares/error.ts
@@ -0,0 +1,9 @@
+import type { Request, Response, NextFunction } from "express";
+
+// TODO
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
